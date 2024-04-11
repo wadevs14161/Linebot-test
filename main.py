5779919ef3d48db52a947504cd22069ad5f25519
LUO WEN SHIN <wadevs14161@gmail.com>
2024-04-11 11:49:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -81,9 +81,7 @@ def find_product():
                     messages=[TextMessage(text=reply1),
                               TextMessage(text=reply2)]))
             else:
-                reply1 = "商品連結:\n %s\n商品價格: %s日圓\n折合台幣: %s元" % (result[1], result[2], result[3])
-                if result[4]:
-                    reply1 += "\n臺灣官網售價: %s元" % result[4][2]
+                reply1 = "商品連結:\n %s\n商品價格: %s日圓\n折合台幣: %s元\n臺灣官網售價: %s元" % (result[1], result[2], result[3], result[4][2])
                 available_dict = {}
                 for item in result[5]:
                     if item['stock'] != 'STOCK_OUT' and item['color'] not in available_dict:
